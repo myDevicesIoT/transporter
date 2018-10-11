70d23e9ee35edb79b41b15eed85a39af14e4d256
tdeleanu <teo.deleanu@gmail.com>
2018-10-11 21:52:30 +0300

    feat: azure event hubs transport layer
    Added support for eventhubs.
    TODO: requires test.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ transporter.Publisher = require('./lib/publisher').Publisher;
 transporter.Redis = require('./lib/redis').Redis;
 transporter.Kafka = require('./lib/kafka').Kafka;
 transporter.Kinesis = require('./lib/kinesis').Kinesis;
+transporter.EventHubs = require('./lib/event-hubs').EventHubs;
 
 const defaultPublisher = new transporter.Publisher({
   transports: []
